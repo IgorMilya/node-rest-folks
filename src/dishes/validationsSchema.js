f74ccbb4440c5700953921ef9d090e60d842eca2
Julia <bullet.jul@gmail.com>
2023-05-12 14:02:14 +0300

    5

--- src/dishes/validationsSchema.js
+++ src/dishes/validationsSchema.js
@@ -7,7 +7,7 @@ export const dishesValidateSchema = Joi.object({
     description: Joi.string().required().min(10),
     category: Joi.string().required().min(3),
     subcategory: Joi.string().min(3),
-    weight: Joi.number().required().min(100),
+    weight: Joi.number().required().positive(),
     bonus: Joi.number().positive(),
     additionalFood: Joi.array().items(
         Joi.object({
--- src/orders/OrdersModel.js
+++ src/orders/OrdersModel.js
@@ -1,40 +1,89 @@
 import mongoose from 'mongoose';
 
-const dishes = new mongoose.Schema({
-    title: { type: String, required: true },
-    price: { type: Number, required: true },
-    amount: { type: Number, required: true },
-});
+const dishes = new mongoose.Schema(
+    {
+        title: { type: String, required: true },
+        price: { type: Number, required: true },
+        amount: { type: Number, required: true },
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+        id: true,
+        toJSON: {
+            transform(doc, ret) {
+                ret.id = ret._id;
+                delete ret._id;
+            },
+        },
+    }
+);
 
-const additionalFood = new mongoose.Schema({
-    title: { type: String, required: true },
-    price: { type: Number, required: true },
-    amount: { type: Number, required: true },
-});
+const additionalFood = new mongoose.Schema(
+    {
+        title: { type: String, required: true },
+        price: { type: Number, required: true },
+        amount: { type: Number, required: true },
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+        id: true,
+        toJSON: {
+            transform(doc, ret) {
+                ret.id = ret._id;
+                delete ret._id;
+            },
+        },
+    }
+);
 
-const deliveryDetails = new mongoose.Schema({
-    phone: { type: String, required: true },
-    name: { type: String, required: true },
-    addresses: { type: String, required: true },
-    email: String,
-});
+const deliveryDetails = new mongoose.Schema(
+    {
+        phone: { type: String, required: true },
+        name: { type: String, required: true },
+        addresses: { type: String, required: true },
+        email: String,
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+        id: true,
+        toJSON: {
+            transform(doc, ret) {
+                ret.id = ret._id;
+                delete ret._id;
+            },
+        },
+    }
+);
 
-export const Order = new mongoose.Schema({
-    status: { type: Boolean, required: true },
-    diningOptions: { type: String, required: true },
-    orderNumber: { type: String, required: true },
-    totalPrice: { type: Number, required: true },
-    paymentMethod: { type: String, required: true },
-    tipAmount: Number,
-    tableTitle: String,
-    dishes: [dishes],
-    additionalFood: [additionalFood],
-    deliveryDetails: deliveryDetails,
-    email: String,
-    notes: String,
-});
+export const Order = new mongoose.Schema(
+    {
+        status: { type: Boolean, required: true },
+        diningOptions: { type: String, required: true },
+        orderNumber: { type: String, required: true },
+        totalPrice: { type: Number, required: true },
+        paymentMethod: { type: String, required: true },
+        tipAmount: Number,
+        tableTitle: String,
+        dishes: [dishes],
+        additionalFood: [additionalFood],
+        deliveryDetails: deliveryDetails,
+        email: String,
+        notes: String,
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+        id: true,
+        toJSON: {
+            transform(doc, ret) {
+                ret.id = ret._id;
+                delete ret._id;
+            },
+        },
+    }
+);
 
 export default mongoose.model('OrderDB', Order, 'orders');
-
-//запит на апдейт резерваціії (+ new) і створення ордера
-//запит на апдейт резерваціії (змінити мтатус) і закрити ордера (status - тру)
