ab471eb113865011bacca17d1d756eff75b21649
artemnadosha <artemnadosha@gmail.com>
2023-05-21 18:06:32 +0300

    add static

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import path, {resolve} from 'path'
 import { tablesDefaultPath, tablesRouter } from './src/tables/routes.js';
 import { orderDefaultPath, orderRouter } from './src/orders/orderRouter.js';
 import { dishesDefaultPath, dishRouter } from './src/dishes/dishesRouter.js';
@@ -9,6 +10,13 @@ export const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(resolve(process.cwd(), 'static')));
+
+app.get("/static/:dirName/:fileName", (req, res) => {
+    const { dirName, fileName } = req.params;
+    const filePath = path.join(process.cwd(), 'static', dirName, fileName);
+    res.sendFile(filePath);
+});
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
