ac721c22fbaf0d5aec79ede3c726b05a00754447
Julia <bullet.jul@gmail.com>
2023-05-22 00:29:45 +0300

    change subcategory search

--- src/dishes/DishesService.js
+++ src/dishes/DishesService.js
@@ -9,13 +9,24 @@ const create = async dish => {
 
 const getAll = async ({ q, category, subcategory }) => {
     const { _id: categoryID } = !!category && (await CategoryDAL.findOne(category, 'id'));
-    const { _id: subcategoryID } = !!subcategory && (await CategoryDAL.findOne(subcategory, 'id'));
+
+    let subcategoryArr = [];
+
+    if (!!subcategory) {
+        await Promise.all(
+            subcategory.split(',').map(async subcategory => {
+                const { _id: subcategoryID } = await CategoryDAL.findOne(subcategory, 'id');
+
+                subcategoryArr.push({ subcategoryID });
+            })
+        );
+    }
 
     const findValues = {
         type: 'dish',
         ...(!!q && { $or: [{ title: new RegExp(q, 'i') }, { description: new RegExp(q, 'i') }] }),
         ...(!!category && { categoryID }),
-        ...(!!subcategory && { subcategoryID }),
+        ...(!!subcategory && { $or: subcategoryArr }),
     };
 
     const dishes = await DishesDAL.findAll(findValues);
