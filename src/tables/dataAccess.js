3775d74c32a22d73fa215c4ea487f32e30b68ee4
Oleksandr Lotko <lotkoolexandr@gmail.com>
2023-05-24 22:05:08 +0300

    tables refactoring

--- src/tables/dataAccess.js
+++ src/tables/dataAccess.js
@@ -1,23 +1,27 @@
-import { TableModel } from './TableModel.js';
+import { TableModel } from './TableModel.js'
 
-const getAllTables = async () => TableModel.find();
+const getAllTables = async () => TableModel.find()
 
-const addNewTable = async body => new TableModel(body).save();
+const addNewTable = async body => new TableModel(body).save()
 
-const getTable = async id => TableModel.findById(id);
+const getTable = async id => TableModel.findById(id)
 
-const updateTableInfo = async (id, data) => TableModel.findByIdAndUpdate(id, data);
-const deleteTableDataAccess = async id => TableModel.findByIdAndDelete(id);
+const getTableByName = async tableNumber => TableModel.findOne({ number: tableNumber })
+
+const updateTableInfo = async (id, data) => TableModel.findByIdAndUpdate(id, data)
+
+const deleteTableDataAccess = async id => TableModel.findByIdAndDelete(id)
 
 const removeReservation = async (filterField, fieldToRemove) => {
-    return TableModel.updateOne(filterField, fieldToRemove);
-};
+  return TableModel.updateOne(filterField, fieldToRemove)
+}
 
 export const dataAccess = {
-    getAllTables,
-    addNewTable,
-    getTable,
-    updateTableInfo,
-    deleteTableDataAccess,
-    removeReservation,
-};
+  getAllTables,
+  addNewTable,
+  getTable,
+  getTableByName,
+  updateTableInfo,
+  deleteTableDataAccess,
+  removeReservation,
+}
