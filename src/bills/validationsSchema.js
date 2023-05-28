78c20e88775bfaca838b7e8df8e3362fcef33017
Julia <bullet.jul@gmail.com>
2023-05-28 11:54:30 +0300

    valid field fix

--- src/bills/validationsSchema.js
+++ src/bills/validationsSchema.js
@@ -1,26 +1,24 @@
 import Joi from 'joi';
 
 export const billsValidateSchema = Joi.object({
-  orderID: Joi.string().required(),
-  orderType: Joi.string()
-    .required()
-    .pattern(/^takeAway|delivery|dineIn$/),
-  orderNumber: Joi.string().required(),
-  table: Joi.string(),
-  description: Joi.string(),
-  status: Joi.string()
-    .required()
-    .pattern(/^opened|closed$/),
-  totalPrice: Joi.number().required().positive(),
-  tip: Joi.number().positive(),
-  email: Joi.string().email(),
-  paymentMethod: Joi.string(),
-  dishes: Joi.array().items(
-    Joi.object({
-      dishID: Joi.string().required().min(3),
-      amount: Joi.number().required().integer().positive(),
-      title: Joi.string().min(3),
-      price: Joi.number().required().positive(),
-    })
-  ),
+    orderID: Joi.string().required(),
+    orderType: Joi.string()
+        .required()
+        .pattern(/^takeAway|delivery|dineIn$/),
+    orderNumber: Joi.number().required(),
+    table: Joi.string(),
+    description: Joi.string(),
+    status: Joi.string().pattern(/^opened|closed$/),
+    totalPrice: Joi.number().required().positive(),
+    tip: Joi.number().positive(),
+    email: Joi.string().email(),
+    paymentMethod: Joi.string(),
+    dishes: Joi.array().items(
+        Joi.object({
+            dishID: Joi.string().required().min(3),
+            amount: Joi.number().required().integer().positive(),
+            title: Joi.string().min(3),
+            price: Joi.number().required().positive(),
+        })
+    ),
 });
