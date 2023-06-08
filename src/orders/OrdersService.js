69530069acc652e6055ec28653d4796327fb0e05
artemnadosha <artemnadosha@gmail.com>
2023-06-08 15:58:00 +0300

    delete log

--- src/orders/OrdersService.js
+++ src/orders/OrdersService.js
@@ -1,56 +1,61 @@
-import { OrderDAL } from './OrdersDAL.js';
-import { findAdditionalFields } from './utils.js';
+import { OrderDAL } from "./OrdersDAL.js";
+import { findAdditionalFields } from "./utils.js";
 
-const create = async order => {
-    const totalDishInfo = await findAdditionalFields(order);
-    console.log(totalDishInfo);
-    const createdOrder = await OrderDAL.create({ ...order, dishes: totalDishInfo });
-    return createdOrder;
+const create = async (order) => {
+  const totalDishInfo = await findAdditionalFields(order);
+  const createdOrder = await OrderDAL.create({
+    ...order,
+    dishes: totalDishInfo,
+  });
+  return createdOrder;
 };
 
-const getAll = async query => {
-    if (query.orderType) {
-        query.orderType = { $in: query.orderType?.split(',') };
-    }
+const getAll = async (query) => {
+  if (query.orderType) {
+    query.orderType = { $in: query.orderType?.split(",") };
+  }
 
-    const { page, limit, ...paramsQuery } = query;
+  const { page, limit, ...paramsQuery } = query;
 
-    const findValue = { $and: [paramsQuery, { status: 'opened' }] };
+  const findValue = { $and: [paramsQuery, { status: "opened" }] };
 
-    const orders = await OrderDAL.findAll({ page, limit, findValue });
-    return orders;
+  const orders = await OrderDAL.findAll({ page, limit, findValue });
+  return orders;
 };
 
-const getOne = async id => {
-    if (!id) {
-        throw new Error('ID was not set');
-    }
-    const order = await OrderDAL.findByID(id);
-    return order;
+const getOne = async (id) => {
+  if (!id) {
+    throw new Error("ID was not set");
+  }
+  const order = await OrderDAL.findByID(id);
+  return order;
 };
 
-const update = async order => {
-    if (!order.id) {
-        throw new Error('ID was not set');
-    }
-    const totalDishInfo = await findAdditionalFields(order);
-    const updateOrder = await OrderDAL.update({ ...order, dishes: totalDishInfo });
-    return updateOrder;
+const update = async (order) => {
+  if (!order.id) {
+    throw new Error("ID was not set");
+  }
+  const totalDishInfo = await findAdditionalFields(order);
+  const updateOrder = await OrderDAL.update({
+    ...order,
+    dishes: totalDishInfo,
+  });
+  return updateOrder;
 };
 
-const deleteOrder = async id => {
-    if (!id) {
-        throw new Error('ID was not set');
-    }
-    const order = await OrderDAL.deleteOrder(id);
+const deleteOrder = async (id) => {
+  if (!id) {
+    throw new Error("ID was not set");
+  }
+  const order = await OrderDAL.deleteOrder(id);
 
-    return order;
+  return order;
 };
 
 export const OrderService = {
-    create,
-    getAll,
-    getOne,
-    update,
-    deleteOrder,
+  create,
+  getAll,
+  getOne,
+  update,
+  deleteOrder,
 };
