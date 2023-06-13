e4b072ac4114657aa2b1e6494203f23b34ab5168
Julia <bullet.jul@gmail.com>
2023-06-14 01:11:32 +0300

    гзв ишді

--- src/bills/utils.js
+++ src/bills/utils.js
@@ -1,47 +1,53 @@
 export const getBillsDATA = [
-  {
-    $lookup: {
-      from: "orders",
-      localField: "orderID",
-      foreignField: "_id",
-      as: "ordersData",
+    {
+        $lookup: {
+            from: 'orders',
+            localField: 'orderID',
+            foreignField: '_id',
+            as: 'ordersData',
+        },
     },
-  },
-  {
-    $unwind: "$ordersData",
-  },
-  {
-    $addFields: {
-      id: "$_id",
-      orderType: "$ordersData.orderType",
-      orderNumber: "$ordersData.orderNumber",
-      table: "$ordersData.table",
-      description: "$ordersData.description",
-      user: "$ordersData.user",
-      dishes: "$ordersData.dishes",
+    {
+        $unwind: '$ordersData',
     },
-  },
-  {
-    $project: {
-      _id: 0,
-      ordersData: 0,
-      dishes: {
-        _id: 0,
-      },
+    {
+        $addFields: {
+            id: '$_id',
+            orderType: '$ordersData.orderType',
+            orderNumber: '$ordersData.orderNumber',
+            table: '$ordersData.table',
+            description: '$ordersData.description',
+            user: '$ordersData.user',
+            dishes: '$ordersData.dishes',
+        },
     },
-  },
+    {
+        $lookup: {
+            from: 'users',
+            localField: 'user',
+            foreignField: '_id',
+            as: 'userData',
+        },
+    },
+    {
+        $project: {
+            _id: 0,
+            ordersData: 0,
+            dishes: {
+                _id: 0,
+            },
+        },
+    },
 ];
 
 export const checkTotalPrice = ({ dishes, totalPrice }) => {
-  let calculatedTotalPrice = 0;
+    let calculatedTotalPrice = 0;
 
-  dishes.map(({ price, amount }) => (calculatedTotalPrice += price * amount));
+    dishes.map(({ price, amount }) => (calculatedTotalPrice += price * amount));
 
-  const calculatedPriceWithTenPercent = Math.round(
-    calculatedTotalPrice * 0.1 + calculatedTotalPrice
-  );
+    const calculatedPriceWithTenPercent = Math.round(calculatedTotalPrice * 0.1 + calculatedTotalPrice);
 
-  if (totalPrice !== calculatedPriceWithTenPercent) {
-    throw new Error("it isn`t correct totalPrice");
-  }
+    if (totalPrice !== calculatedPriceWithTenPercent) {
+        throw new Error('it isn`t correct totalPrice');
+    }
 };
