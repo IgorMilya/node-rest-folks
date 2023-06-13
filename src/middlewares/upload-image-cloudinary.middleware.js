23336f1627b5bdfffd2ad7f23c861ad59cd4799a
artemnadosha <artemnadosha@gmail.com>
2023-06-13 19:24:04 +0300

    fixed bug

--- src/middlewares/upload-image-cloudinary.middleware.js
+++ src/middlewares/upload-image-cloudinary.middleware.js
@@ -4,7 +4,7 @@ import { unlinkSync } from "fs";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, "static/images/");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
